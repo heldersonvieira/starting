63bcc166ad86fbc28a235e9f5ce98bc57afe17c9
heldersonvieira <vieirahelder.fort@gmail.com>
2020-10-21 12:40:39 -0300

    Criado script para calcular o IMC e aplicado estilos a tabela e a pagina

---
+++ calcula-imc/scripts/calcula-imc.js
@@ -0,0 +1,50 @@
+var pacientes = document.querySelectorAll('.paciente');
+
+/* PERCORRENDO OS PARANTES PARA ARMAZENAR NAS VARIÁVIES OS VALORES DOS ELEMENTOS HTML */
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+    var nome = paciente.querySelector('.info-nome').textContent;
+    var peso = paciente.querySelector('.info-peso').textContent;
+    var altura = paciente.querySelector('.info-altura').textContent;
+    var imcTd = paciente.querySelector('.info-imc');
+    var imc = calculaIMC(peso, altura);
+
+    var pesoValido = validaPeso(peso);
+    var alturaValida = validaAltura(altura);
+
+    if (pesoValido && altura) {
+        imcTd.textContent = imc;
+    }
+
+    if (!pesoValido) {
+        imcTd.textContent = 'Peso inválido';
+        imcTd.classList.add('dados-invalidos');
+    }
+
+    if (!alturaValida) {
+        imcTd.textContent = 'Altua inválida';
+        imcTd.classList.add('dados-invalidos')
+    }   
+}
+
+/*  FUNÇÕES */
+function calculaIMC (peso, altura) {
+    var imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
+
+function validaPeso (peso) {
+    if (peso > 0 && peso < 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura (altura) {
+    if (altura > 0 && altura < 3.00) {
+        return true;
+    } else {
+        return false;
+    }
+}
