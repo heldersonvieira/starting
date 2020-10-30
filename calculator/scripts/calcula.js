bff8289697fb61184b8cd8b44c920cfe62db0106
heldersonvieira <vieirahelder.fort@gmail.com>
2020-10-30 10:32:31 -0300

    Funções de calculo e iniciado a estilização da pagina

---
+++ calculator/scripts/calcula.js
@@ -0,0 +1,74 @@
+var button = document.querySelector('.button-calc');
+var operators = document.querySelectorAll('.operator');
+
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+    var firstNumber = parseFloat(document.querySelector('#input1').value);
+    var secondNumber = parseFloat(document.querySelector('#input2').value);
+    var showResult = document.querySelector('.result');
+    var result = calcNum(firstNumber, secondNumber, operators);
+    
+    showOperator(operators);
+    showResult.value = result;    
+
+    console.log(firstNumber);
+});
+
+
+function showOperator(operators) {
+    var show = document.querySelector('.show-operator');
+    operators.forEach(operator => {
+        if (operator.checked == true) {
+            
+            if (operator.id == 'sum') {
+                show.textContent = '+';
+            } else if (operator.id == 'subtraction') {
+                show.textContent = '-';
+            } else if (operator.id == 'multiplication') {
+                show.textContent = '*';
+            } else if (operator.id == 'division') {
+                show.textContent = '/';
+            }
+        }
+    })
+    
+    return show.textContent;
+}
+
+function calcNum(num1, num2, operators) {
+    var result = 0;
+    operators.forEach(operator => {
+        if (operator.checked == true) {
+            if (operator.id == 'sum') {
+                result = sum(num1, num2);
+            } else if (operator.id == 'subtraction') {
+                result = subtract(num1, num2);
+            } else if (operator.id == 'multiplication') {
+                result = multiply(num1, num2);
+            } else if (operator.id == 'division') {
+                result = divide(num1, num2);
+            }
+        }
+    });
+
+    return result;
+}
+
+
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
--- calculator/scripts/operacoes.js
+++
@@ -1,5 +0,0 @@
-function sum (num1, num2) {
-    var sum = num1 + num2;
-
-    return sum;
-}
