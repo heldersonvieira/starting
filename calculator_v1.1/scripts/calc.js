14aee2d65c5f10090a3a9d5bda58f1b9ca787ca9
heldersonvieira <vieirahelder.fort@gmail.com>
2020-11-09 17:31:21 -0300

    implementado novas funções

--- calculator_v1.1/scripts/calc.js
+++ calculator_v1.1/scripts/calc.js
@@ -1,38 +1,42 @@
 var display = document.calculator.displayNumber;
 var number1 = 0;
 var number2 = 0;
-var pendingOpe = '';
-var buttons = document.querySelector('table');
+var pendingOpe = true;
 
-buttons.addEventListener('click', function(event) {
-    if (event.target.type == 'button') {
-        
-    }
-})
-
-
-// FUNÇÃO PARA MOSTRAR NO DISPLAY OS VALORES CAPTURADOS
-function showValue(num) {
-    var displayNumber = document.calculator.displayNumber;
-   
-    return displayNumber.value = displayNumber.value + value;
+function clearCalc() {
+    display.value = '';
+    number1 = 0; 
+    number2 = 0;
+    pendingOpe = true;
 }
 
+function calcResult(operator) {
+    var result = document.calculator.displayResult;
+    if (operator = '+') {
+        result.value = parseFloat(number1) + parseFloat(number2);
+    } else if (operator = '-') {
+        result.value = parseFloat(number1) - parseFloat(number2);
+    } else if (operator = '/') {
+        result.value = parseFloat(number1) / parseFloat(number2);
+    } else if (operator = '*') {
+        result.value = parseFloat(number1) * parseFloat(number2);
+    } else {
+        result.value = 'Operação inválida'
+    }
+}
 
 function receivesNumber(num) {
-
-    if (display.value == '') {
-        display.value = num;
-    } else if (display.value != '' && display.value != 'undefined') {
+    if (pendingOpe) {
+        display.value += num; 
+        number1 = display.value;   
+    } else {
         display.value += num;
-    }   
+        number2 = display.value;
+    }
+    console.log(number1, number2);
 }
 
-function operation(ope) {
-    var result = document.calculator.displayResult;
-    if (ope == '+') {
-        
-        
-    }
-        
+function receivesOperator(ope) {
+    display.value = '';
+    pendingOpe = false;
 }
